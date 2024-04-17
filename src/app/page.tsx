e3e0bf82f5af0e1a7b28b2214fd2e972c18b92f9
deepak76901 <deepakb76901@gmail.com>
2024-04-17 12:42:39 +0530

    testimonial, sticky scroll added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import FeaturedSection from "@/components/FeaturedSection";
+import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[10]">
       {/** bg-grid will active after adding a plugin */}
-      <h1 className="text-3xl text-center">Home Page</h1>
+      <HeroSection/>
+      <FeaturedSection/>
+      <WhyChooseUs/>
+      <TestimonialCards/>
     </main>
   );
 }
